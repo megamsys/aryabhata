8eba9a84714a8d2beac79aa71d14413efcc87c3c
thomasalrin <alrin@megam.co.in>
2013-07-16 15:23:16 +0530

    Production test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ GA.script_source = "('https:' == document.location.protocol ? 'https://' : 'http
   config.eager_load = true
 
   # Compress JavaScripts and CSS
-  #config.assets.compress = true
+  config.assets.compress = true
   config.assets.js_compressor = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
@@ -53,8 +53,8 @@ GA.script_source = "('https:' == document.location.protocol ? 'https://' : 'http
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-   config.assets.precompile += %w( search.js )
-
+   #config.assets.precompile += %w( search.js )
+  #config.action_dispatch.best_standards_support = :builtin
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
@@ -71,4 +71,5 @@ GA.script_source = "('https:' == document.location.protocol ? 'https://' : 'http
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.assets.debug = true
 end
